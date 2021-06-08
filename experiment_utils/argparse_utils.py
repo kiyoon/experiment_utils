3adea0c0c78a0f6968a5aa3f7deb69624804b89c
Kiyoon Kim <kiyoon.kim@ed.ac.uk>
2021-06-08 12:49:43 +0100

    config channels, telegram send numpy as photo, GIF

--- experiment_utils/argparse_utils.py
+++ experiment_utils/argparse_utils.py
@@ -1,6 +1,9 @@
-def add_exp_arguments(parser, dataset_choices=None, model_choices=None, root_default=None, dataset_default=None, model_default=None, name_default=None):
+def add_exp_arguments(parser, dataset_choices=None, model_choices=None, root_default=None, dataset_default=None, model_default=None, name_default=None, dataset_channel_choices=None, model_channel_choices=None, exp_channel_choices=None, dataset_channel_default=None, model_channel_default=None, exp_channel_default=None):
 
     parser.add_argument("-R", "--experiment_root", type=str, required=root_default is None, default=root_default, help="A directory to save the experiment.")
     parser.add_argument("-D", "--dataset", type=str, required=dataset_default is None, default=dataset_default, choices=dataset_choices, help="Dataset to use.")
     parser.add_argument("-M", "--model", type=str, required=model_default is None, default=model_default, choices=model_choices, help="Model architecture to use.")
     parser.add_argument("-N", "--experiment_name", type=str, required=name_default is None, default=name_default, help="A directory name in experiment_root/dataset to save/access the model and logs.")
+    parser.add_argument("-c:d", "--dataset_channel", type=str, default=dataset_channel_default, choices=dataset_channel_choices, help="Dataset config channel.")
+    parser.add_argument("-c:m", "--model_channel", type=str, default=model_channel_default, choices=model_channel_choices, help="Model config channel.")
+    parser.add_argument("-c:e", "--experiment_channel", type=str, default=exp_channel_default, choices=exp_channel_choices, help="Dataset config channel.")
--- experiment_utils/telegram_post.py
+++ experiment_utils/telegram_post.py
@@ -6,7 +6,11 @@
 telegram_token = ""
 telegram_chat_ids = [""]
 
-
+try:
+    from PIL import Image
+except ImportError:
+    # backward compatibility for the ones who don't need sending numpy photos
+    pass
 
 import requests
 import io
@@ -42,35 +46,68 @@ def send_text_with_title(telegram_token, chat_id, title, body):
     return tg_request
 
 
-def _send_photo_bytes(telegram_token, chat_id, bytes_io):
+def _send_photo_bytes(telegram_token, chat_id, bytes_io, caption=None):
     """Send photo in open() or io.BytesIO form.
     """
     url = "https://api.telegram.org/bot{}/sendPhoto".format(telegram_token);
     files = {'photo': bytes_io}
     data = {'chat_id' : chat_id}
+    if caption is not None:
+        data['caption'] = caption
     r= requests.post(url, files=files, data=data)
     return r
 
 
-def send_photo(telegram_token, chat_id, img_path):
+def send_photo(telegram_token, chat_id, img_path, caption=None):
     photo = open(img_path, 'rb')
-    return _send_photo_bytes(telegram_token, chat_id, photo)
+    return _send_photo_bytes(telegram_token, chat_id, photo, caption)
 
+def send_numpy_photo(telegram_token, chat_id, numpy_photo, caption=None):
+    image = Image.fromarray(numpy_photo)
+    photo = io.BytesIO()
+    image.save(photo, format='jpg')
+    photo.seek(0)       # to start reading from the beginning. (After writing, the cursor is at the end)
+    photo.name = 'img.jpg'
+    return _send_photo_bytes(telegram_token, chat_id, photo, caption)
 
-def send_remote_photo(telegram_token, chat_id, img_url):
+def send_remote_photo(telegram_token, chat_id, img_url, caption):
     remote_image = requests.get(img_url)
     photo = io.BytesIO(remote_image.content)
     photo.name = 'img.png'
-    return _send_photo_bytes(telegram_token, chat_id, photo)
+    return _send_photo_bytes(telegram_token, chat_id, photo, caption)
 
 
-def send_matplotlib_fig(telegram_token, chat_id, fig):
+def send_matplotlib_fig(telegram_token, chat_id, fig, caption=None):
     photo = io.BytesIO()
     fig.savefig(photo, format='png')
     photo.seek(0)       # to start reading from the beginning. (After writing, the cursor is at the end)
     photo.name = 'img.png'
-    return _send_photo_bytes(telegram_token, chat_id, photo)
+    return _send_photo_bytes(telegram_token, chat_id, photo, caption)
 
+def _send_gif_bytes(telegram_token, chat_id, bytes_io, caption=None):
+    """Send GIF in open() or io.BytesIO form.
+    """
+    url = "https://api.telegram.org/bot{}/sendAnimation".format(telegram_token);
+    files = {'animation': bytes_io}
+    data = {'chat_id' : chat_id}
+    if caption is not None:
+        data['caption'] = caption
+
+    r= requests.post(url, files=files, data=data)
+    return r
+
+def send_numpy_video_as_gif(telegram_token, chat_id, video, caption=None, optimize=True, duration=100, loop=0 ):
+    # T, H, W, C = video.shape
+    gif_frames = []
+    for gif_frame in video:
+        gif_frames.append(Image.fromarray(gif_frame))
+
+    gif_bytes = io.BytesIO()
+    gif_frames[0].save(gif_bytes, format='gif', save_all=True, append_images=gif_frames[1:], optimize=optimize, duration=duration, loop=loop)    
+    gif_bytes.seek(0)       # to start reading from the beginning. (After writing, the cursor is at the end)
+    gif_bytes.name = 'img.gif'
+
+    return _send_gif_bytes(telegram_token, chat_id, gif_bytes, caption)
 
 def send_document(telegram_token, chat_id, file_path):
     file_to_send = open(file_path, 'rb')
